972fe0b50672eced757f8992a98f5728543b8281
Francis Authority <francis.i.authority@gmail.com>
2024-09-03 20:43:02 +0200

    ...

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -49,7 +49,7 @@ const page = () => {
                   <Image 
                     src={pastP.img} 
                     alt={`${pastP.name}`}
-                    className='xs:w-48 sm:w-80 h-auto rounded-tr-xl rounded-br-xl'
+                    className='xs:w-52 sm:w-80 h-auto rounded-tr-xl rounded-br-xl'
                     />
               </div>
                 <p className='my-2 xs:text-xs xr:text-sm sm:text-base xr:text-justify'>
