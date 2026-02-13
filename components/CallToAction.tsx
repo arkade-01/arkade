62cb214eeeaab66eb43805732af0439fa816ff8f
arkade-01 <noabiliaminfirst@gmail.com>
2026-02-13 22:15:04 +0100

    fix: change Download CV to View Resume

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -35,7 +35,7 @@ const CallToAction = () => {
             </div>
           </div>
           <div className='flex xs:flex-col xs:items-end sm:flex-row sm:items-center sm:justify-end w-full'>
-            <Button text='Download CV' onClick={handleCVDownload}/>
+            <Button text='View Resume' onClick={handleCVDownload}/>
             <div className='flex xs:justify-center xs:mr-1 sm:mr-0  sm:justify-end xs:mt-2 sm:mt-0 sm:ml-2 md:ml-3 lg:ml-5'>
                 {navSocials.map((navSocial) => (
                   <Link
